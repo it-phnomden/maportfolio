8ee41ea737d8241258e7effb10bcb4375c9239b8
PiSethSeam <7piseth@gmail.com>
2023-04-19 13:17:19 +0700

    removed hover effect

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Experience = () => {
     return <>
-        <h1 className="bg-green-500 w-fit h-full">This is experience page</h1>
+        <h1 className=" h-full">This is experience page</h1>
     </>
 }
 export default Experience;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -44,7 +44,7 @@ const NavBar = () => {
     return( 
     <>
         <div className={`w-screen h-[50px] px-5 ${themeMode} flex justify-between items-center fixed`}>
-            <h1 className="font-signature font-bold text-2xl py-2 cursor-pointer transform duration-300  hover:tracking-widest">PiSeth</h1>
+            <h1 className="font-signature font-bold text-2xl py-2 cursor-pointer transform duration-100  active:tracking-widest">PiSeth</h1>
            
             <div className="rounded-full flex absolute top-1 left-[calc(50%-41px)] bg-[rgb(255,255,255)]  dark:bg-[rgb(43,47,66)]">
                 <div 
@@ -52,7 +52,7 @@ const NavBar = () => {
                         setDarkMode(false)
                         setThemeMode(lightTheme);
                     }}
-                    className={`py-3 px-4 cursor-pointer rounded-l-full ${darkMode?'opacity-50':'opacity-100'} hover:opacity-20`}>
+                    className={`py-3 px-4 cursor-pointer rounded-l-full ${darkMode?'opacity-50':'opacity-100'} hover:opacity-20 max-md:hover:opacity-100`}>
                         <BsFillSunFill size={17} />
                 </div>
                 <div 
@@ -60,7 +60,7 @@ const NavBar = () => {
                         setDarkMode(true);
                         setThemeMode(darkTheme);
                     }} 
-                    className={`p-3 cursor-pointer rounded-r-full ${darkMode?'opacity-100':'opacity-50'} hover:opacity-20`}>
+                    className={`p-3 cursor-pointer rounded-r-full ${darkMode?'opacity-100':'opacity-50'} hover:opacity-20 max-md:hover:opacity-100`}>
                     
                     <BsMoonStarsFill size={17} />
                 </div>
@@ -70,24 +70,24 @@ const NavBar = () => {
                 { links.map( ({id, link}) => (
                     <li 
                         key={id}
-                        className={`px-5 py-2 cursor-pointer capitalize rounded-l duration-100 active:scale-110 hover:opacity-70`}
+                        className={`px-5 py-2 cursor-pointer capitalize rounded-l duration-100 active:scale-110 hover:opacity-70 max-md:hover:opacity-100`}
                         >{link}
                     </li>
                 ))
                 }
             </ul>
 
-             <div className="cursor-pointer hidden max-md:flex z-20 hover:opacity-50"
+             <div className="cursor-pointer hidden max-md:flex z-20 hover:opacity-50 max-md:hover:opacity-100"
                   onClick={()=> setNav(!nav)} >
                 <Hamburger bgColor='bg-black dark:bg-white' isOpen={nav} />
             </div>
            
             <ul className={`${themeMode} flex flex-col absolute right-0 top-0 w-full h-screen justify-center items-center
-              z-10 transition ease-in-out duration-500 ${nav?'translate-y-[0vh]':'-translate-y-[100vh]'}`} >
+              z-10 transition ease-in-out duration-400 ${nav?'translate-y-[0vh] overflow-y-hidden':'-translate-y-[100vh]'}`} >
             { links.map( ({id, link}) => (
                 <li 
                     key={id}
-                    className={`px-5 py-5 cursor-pointer capitalize text-2xl rounded-l duration-100 active:scale-110 hover:opacity-70`}
+                    className={`px-5 py-5 cursor-pointer capitalize text-2xl rounded-l duration-100 active:scale-110 hover:opacity-70 max-md:hover:opacity-100`}
                     >{link}
                 </li>
             ))
