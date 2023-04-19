66a58d2bc1e03dec7f7a773eb4d12069cfb4f392
PiSethSeam <7piseth@gmail.com>
2023-04-19 08:59:02 +0700

    disabled hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'hover-hover': {
+            'raw': '(hover: hover)'
+        },
+    } },
     fontFamily: {
       signature: ["Great Vibes"],
     }
