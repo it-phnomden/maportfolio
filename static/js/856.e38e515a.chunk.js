ac4e9dd0285d21657c0c0dc8ab80ca4e665ac5ce
PiSethSeam <7piseth@gmail.com>
2023-05-06 20:50:03 +0700

    Updates

--- static/js/856.e38e515a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmaportfolio=self.webpackChunkmaportfolio||[]).push([[856],{856:function(t,a,n){n.d(a,{Bqb:function(){return r}});var o=n(983);function r(t){return(0,o.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"}}]})(t)}}}]);
-//# sourceMappingURL=856.e38e515a.chunk.js.map
