ced70ad0b41861d26c54ee07f450c37e0ec9f459
PiSethSeam <7piseth@gmail.com>
2023-06-09 17:09:17 +0700

    Added experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,80 @@
 import React from "react";
+import html from "../assets/experiences/html.png"
+import css from "../assets/experiences/css.png"
+import js from "../assets/experiences/javascript.png"
+import tailwind from "../assets/experiences/tailwind.png"
+import react from "../assets/experiences/react.png"
+import node from "../assets/experiences/node.png"
+import flutter from "../assets/experiences/flutter.png"
+import csharp from "../assets/experiences/csharp.png"
+import java from "../assets/experiences/java.png"
+import springboot from "../assets/experiences/spring-boot.png"
+import sql from "../assets/experiences/sql.png"
+import github from "../assets/experiences/github.png"
+
+const skills = [
+  {
+    id: 1,
+    src: html,
+    title: "HTML",
+    style: "shadow-[#E95626]"
+  }, {
+    id: 2,
+    src: css,
+    title: "CSS",
+    style: "shadow-[#0277BD]"
+  }, {
+    id: 3,
+    src: js,
+    title: "Javascript",
+    style: "shadow-[#FFD600]"
+  }, {
+    id: 4,
+    src: tailwind,
+    title: "TailWindCSS",
+    style: "shadow-[#44A8B3]"
+  }, {
+    id: 5,
+    src: react,
+    title: "ReactJS",
+    style: "shadow-[#00D7FE]"
+  }, {
+    id: 6,
+    src: node,
+    title: "Node",
+    style: "shadow-[#8BC74A]"
+  }, {
+    id: 7,
+    src: flutter,
+    title: "Flutter",
+    style: "shadow-[#40C4FF]"
+  }, {
+    id: 8,
+    src: csharp,
+    title: "C#",
+    style: "shadow-[#505D67]"
+  }, {
+    id: 9,
+    src: java,
+    title: "Java",
+    style: "shadow-[#4651EA]"
+  }, {
+    id: 10,
+    src: springboot,
+    title: "Spring-Boot",
+    style: "shadow-[#8BC34A]"
+  }, {
+    id: 11,
+    src: sql,
+    title: "SQL",
+    style: "shadow-[#0072C6]"
+  }, {
+    id: 12,
+    src: github,
+    title: "GitHub",
+    style: "shadow-[#EEEEEE]"
+  },
+]
 
 const Experience = () => {
   return (
@@ -7,14 +83,21 @@ const Experience = () => {
       className="md:h-screen flex flex-col justify-center max-md:pt-[70px]"
     >
       <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-        this is experience page Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Doloremque ipsa repudiandae libero maxime dicta
-        voluptas laboriosam inventore magnam repellat ratione, obcaecati
-        recusandae a. Commodi porro sequi cumque dolorum nesciunt
-        expedita!Loreml Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Alias amet assumenda, placeat dolore quos enim adipisci sequi
-        voluptate veritatis mollitia eos illum nisi omnis sapiente tempora
-        distinctio eius repudiandae odio?
+        <p className="text-xl font-extrabold w-fit border-b-2 mb-2">Experience</p>
+        <p>These are the technologies that I have worked with.</p>
+        <div className="w-full my-5 grid grid-cols-2 sm:grid-cols-4 gap-8">
+          {
+            skills.map(({id, src, title, style}) => (
+              <div 
+              key={id}
+              className={`w-52 flex flex-col items-center shadow-md ${style}`}>
+                <img src={src} alt="html.png" className="w-20" />
+                <p className="text-lg">{title}</p>
+              </div>
+
+            ))
+          }
+        </div>
       </div>
     </div>
   );
