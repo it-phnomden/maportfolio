48a3bd81303b0be3473e44155ba1e36a8c232432
PiSethSeam <7piseth@gmail.com>
2023-06-11 22:58:24 +0700

    Added portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import calculator from "../assets/portfolio/calculator.png";
 
 const Portfolio = () => {
   return (
@@ -7,20 +8,23 @@ const Portfolio = () => {
       className="md:h-screen flex flex-col justify-center max-md:pt-[70px]"
     >
       <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-        This portfolio page Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Vero corrupti quae inventore! Commodi, ipsam culpa aliquam totam
-        reiciendis in iste molestias reprehenderit ratione eius placeat
-        doloribus asperiores ab cum necessitatibus! Lorem ipsum, dolor sit amet
-        consectetur adipisicing elit. In, quia eius perferendis sapiente ipsam
-        quas corporis maiores numquam iusto amet impedit sequi corrupti delectus
-        qui, voluptate pariatur porro mollitia rem. Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Reprehenderit, eligendi exercitationem
-        corporis illo accusantium enim mollitia ducimus vitae tempore sequi quos
-        repellendus excepturi nesciunt unde fuga temporibus ullam dolore maxime.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
-        accusamus eveniet distinctio reiciendis delectus asperiores id, qui
-        cupiditate et quasi nisi temporibus sequi exercitationem, eligendi ad
-        nesciunt rerum magnam explicabo!
+        <p className="text-xl font-extrabold w-fit border-b-2 mb-2">
+          Portfolio
+        </p>
+        <p className="indent-7">
+          Here are some projects that have done before. If you want to see
+          the source code and how does it work you can check out them below.
+        </p>
+        <div
+          className="w-60 rounded-3xl mt-5 bg-[#F4F6F8] dark:bg-[#282C34]
+        p-3"
+        >
+          <img src={calculator} alt="Calculator.png" />
+          <div className="flex justify-evenly">
+            <a href="https://pisethseam.github.io/easy-calculate/">Demo</a>
+            <a href="https://github.com/PiSethSeam/easy-calculate">Code</a>
+          </div>
+        </div>
       </div>
     </div>
   );
