edf7a3bbd70846d55a4ccd01e51284a65464ef8a
PiSethSeam <7piseth@gmail.com>
2023-06-16 17:14:20 +0700

    Updated content about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,8 +18,8 @@ const AboutMe = () => {
           I'm a software engineer with three years of experience in
           developing and maintaining software applications. I have experience in
           designing and implementing software solutions for various clients and
-          projects. I'm proficient in programming languages such as Java,
-          Javascript and C/C++. I graduated from Royal University of Phnom Penh
+          projects. I'm proficient in programming languages such as Java, C#,
+          PHP, Dart, Swift Tyepscript and Javascript. I graduated from Royal University of Phnom Penh
           with a degree in Computer Science and have been working as a Software
           Engineer at Mobile C&C Cambodia Company since June 2020.
         </p>
